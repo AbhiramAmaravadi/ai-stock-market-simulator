5ad21531c5c9f8a1b088b1fcecda08cefad3de9b
Amaravadi <aamaravadi@wisc.edu>
2026-08-13 21:54:58 -0500

    Allow production frotnend cors

--- src/main/java/com/abhiram/stocktrader/config/CorsConfig.java
+++ src/main/java/com/abhiram/stocktrader/config/CorsConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.filter.CorsFilter;config.addAllowedOrigin("https://lovable-insights-omega.vercel.app");
 
 @Configuration
 public class CorsConfig {
@@ -18,6 +18,7 @@ public class CorsConfig {
 
         config.addAllowedOrigin("http://localhost:5173");
         config.addAllowedOrigin("http://localhost:8081");
+        config.addAllowedOrigin("https://lovable-insights-omega.vercel.app");
 
         config.addAllowedHeader("*");
 
